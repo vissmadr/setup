8924d1c3b4e449397a0c5473ce597761644ef366
Doni <vissmadr@pm.me>
2025-10-17 23:17:28 +0300

    update(nvim)

---
+++ home/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,30 @@
+return {
+	"epwalsh/obsidian.nvim",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		dir = "~/core/vault/",
+
+		notes_subdir = "nodes",
+		new_notes_location = "notes_subdir",
+		attachments = { img_folder = "resources" },
+
+		templates = {
+			subdir = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			substitutions = {},
+		},
+
+		ui = { enable = false },
+
+		note_id_func = function(title)
+			return title
+		end,
+
+		disable_frontmatter = true,
+	},
+}
